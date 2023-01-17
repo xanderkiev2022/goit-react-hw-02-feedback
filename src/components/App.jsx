ca16f997fa0bfc3790cff5f5317b03b79fe71b4b
xanderkiev2022 <301019a@gmail.com>
2023-01-17 18:51:27 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 // import Profile from './components/Profile/Profile';
 
 import React, { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 
 export default class Feedback extends Component {
   state = {
@@ -9,8 +11,19 @@ export default class Feedback extends Component {
     bad: 0,
   };
 
+  // {[options]:state[options]+1 }
+
+  countTotalFeedback();
+  countPositiveFeedbackPercentage();
+
   render() {
     const { good, neutral, bad } = this.state;
-    return <div></div>;
+    return (
+      <Section title="">
+        <Statistics good={ } neutral={ } bad={ } total={ } positivePercentage={ }/>
+        <FeedbackOptions options={'good', 'neutral', 'bad' } onLeaveFeedback={ } />
+        <Notification message="There is no feedback" />
+      </Section>
+    );
   }
 }
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => ();
+
+Statistics.propTypes = {
+//   _____: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//     }).isRequired,
+//   ).isRequired,
+};
+
+export default TodoList;
