6369074dc5742a4a8245d27c64d554160307177b
xanderkiev2022 <301019a@gmail.com>
2023-01-17 18:37:56 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,37 +2,15 @@
 
 import React, { Component } from 'react';
 
-
-class Feedback extends Component {
+export default class Feedback extends Component {
   state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-}
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
   render() {
     const { good, neutral, bad } = this.state;
-
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div></div>;
   }
-
-// export const App = () => {
-//   return (
-//     <div>
-//       <Profile
-//         username={user.username}
-//         tag={user.tag}
-//         location={user.location}
-//         avatar={user.avatar}
-//         stats={user.stats}
-//       />
-//       <Statistics title="Upload stats" stats={data} />
-//       <FriendList friends={friends} />;
-//       <TransactionHistory items={transactions} />;
-//     </div>
-//   );
-// };
+}
